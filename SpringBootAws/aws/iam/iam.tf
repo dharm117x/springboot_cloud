de448d0208b2af0b646a39979afe27fd303fccec
Dharmendra kumar <dharm117x@gamil.com>
2026-04-05 18:52:01 +0530

    Udpate files and policy

--- SpringBootAws/aws/iam/iam.tf
+++ SpringBootAws/aws/iam/iam.tf
@@ -32,19 +32,9 @@ resource "aws_iam_policy" "sns_policy" {
   policy = templatefile("${path.module}/policies/sns-policy.json", { topic_arn = var.sns_topic_arn })
 }
 
-resource "aws_iam_policy" "rds_policy" {
-  name   = "rds-ec2-access-policy"
-  policy = templatefile("${path.module}/policies/rds-policy.json", { rds_arn = var.rds_instance_arn })
-}
-
-resource "aws_iam_policy" "ecr_read_policy" {
-  name   = "ecr-read-access-policy"
-  policy = templatefile("${path.module}/policies/ecr-policy.json", { ecr_repos_arn = var.ecr_repository_arn })
-}
-
 resource "aws_iam_policy" "cloudwatch_policy" {
-  name   = "cloudwatch-logs-policy"
-  policy = templatefile("${path.module}/policies/cloudwatch-logs-policy.json", {
+  name   = "cloudwatch-ec2-access-policy"
+  policy = templatefile("${path.module}/policies/cloudwatch-policy.json", {
     log_group_arn = var.log_group_arn
   })
 }
@@ -60,8 +50,6 @@ resource "aws_iam_role_policy_attachment" "attachments" {
   for_each = {
     sqs        = aws_iam_policy.sqs_policy.arn
     sns        = aws_iam_policy.sns_policy.arn
-    rds        = aws_iam_policy.rds_policy.arn
-    ecr_read   = aws_iam_policy.ecr_read_policy.arn
     cloudwatch = aws_iam_policy.cloudwatch_policy.arn
   }
 
--- SpringBootAws/aws/iam/variables.tf
+++ SpringBootAws/aws/iam/variables.tf
@@ -3,9 +3,6 @@ variable "region" {
 }
 
 variable "s3_bucket_arn" {}
-variable "rds_instance_arn" {}
-variable "ecr_repository_arn" {}
-
 variable "sns_topic_arn" {}
 variable "sqs_order_queue_arn" {}
 variable "sqs_user_queue_arn" {}
